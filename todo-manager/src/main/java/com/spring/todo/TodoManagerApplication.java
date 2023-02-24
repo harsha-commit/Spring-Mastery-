2bd60d99d85503898ab5d25056776bb9b80d0e21
harsha-commit <harshavardhan.bashavathini@gmail.com>
2023-02-24 11:30:37 +0530

    New Todo App

---
+++ todo-manager/src/main/java/com/spring/todo/TodoManagerApplication.java
@@ -0,0 +1,14 @@
+package com.spring.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoManagerApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		SpringApplication.run(TodoManagerApplication.class, args);
+	}
+
+}
